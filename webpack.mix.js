a588ddb42415efd830b0ab91891b1e85e43b8dd9
Michael Rook <michael@rook.net.au>
2021-08-09 19:39:09 +1000

    Remove Axios version, well that was short lived.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,3 @@ const mix = require('laravel-mix');
 
 // Native
 mix.js('src/data-fetcher.js', 'dist/data-fetcher.min.js');
-
-// Axios
-mix.js('src/data-fetcher.axios.js', 'dist/data-fetcher.axios.min.js');
