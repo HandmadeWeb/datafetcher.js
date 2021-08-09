6bf1edef9c771215ed49f890d6017d1ccfb3efb5
Yohan <john@inertiainmotion.com.au>
2021-08-09 19:09:11 +1000

    Add error handling
    Swap to async/await (not supporting IE)
    Add babel config to prevent huge transpile

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 All notable changes to `dataFetcher.js` will be documented in this file
 
+## 1.1.0 - 09/08/2021
+
+- Async / await with try catch for error handling
+- Adding a babel config to prevent transpiling into huge filesize
+
 ## 1.0.0 - 08/08/2021 (dd/mm/yyyy)
 
 - initial release
--- src/data-fetcher.js
+++ src/data-fetcher.js
@@ -1,7 +1,10 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll("[data-fetcher]").forEach((data) => {
-        fetch(data.getAttribute('data-fetcher'))
-            .then(response => response.text())
-            .then(html => data.innerHTML = html);
-    });
+window.addEventListener('DOMContentLoaded', async () => {
+  for (let el of document.querySelectorAll("[data-fetcher]")) {
+    try {
+      const res = await fetch(el.dataset.fetcher);
+      el.innerHTML = await res.text()
+    } catch (err) {
+      console.log(err)
+    }
+  }
 });
